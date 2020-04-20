2e8dcc514019a6eddfb10339ba489229de85bfd2
agusrichard <agus.richard21@gmail.com>
2020-04-21 00:35:41 +0700

    Create home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar(props) {
         <span className="link">Home</span>
         <span className="link">About</span>
         <div className="logo-container">
-          <img src={logo} className="logo" />
+          <img src={logo} className="logo" alt="Logo"/>
         </div>
         <span className="link">Projects</span>
         <span className="link">Contact</span>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faFacebookSquare
+} from '@fortawesome/free-brands-svg-icons'
 
 
 export default class Home extends React.Component {
@@ -6,7 +13,14 @@ export default class Home extends React.Component {
     return (
       <div id="home" className="home-bg">
         <div className="home">
-        
+          <p style={{ padding: '0.5rem', margin: 0, marginLeft: -100 }}>Hello, I'm Agus Richard Lubis.</p>
+          <p style={{ padding: '0.5rem', margin: 0, marginRight: -200 }}>I'am a Full Stack Developer</p>
+          <div style={{ marginTop: '1.5rem' }}>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/agusrichard"><FontAwesomeIcon icon={ faGithub } className="home-icon" /></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/agusrichard21/"><FontAwesomeIcon icon={ faInstagram } className="home-icon" /></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/agus-richard/"><FontAwesomeIcon icon={ faLinkedin } className="home-icon" /></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://web.facebook.com/agus.richard.16"><FontAwesomeIcon icon={ faFacebookSquare } className="home-icon" /></a>
+          </div>
         </div>
       </div>
     )
